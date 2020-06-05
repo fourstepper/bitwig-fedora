6390ec68b95515f72cad51e12c8030befdb39c9b
Fourstepper <opletalrobin@posteo.net>
2020-06-05 20:11:43 +0200

    Updated to 3.2 :)

--- bitwig-fedora.sh
+++ bitwig-fedora.sh
@@ -9,7 +9,7 @@
 
 ROOT_UID=0
 E_NOTROOT=87
-VERSION=3.1.3
+VERSION=3.2
 DEFAULT_FILENAME="bitwig-studio-$VERSION.deb"
 DEFAULT_URL="https://downloads.bitwig.com/stable/$VERSION/$DEFAULT_FILENAME"
 INSTALL_LOG="/opt/bitwig-studio/.$DEFAULT_FILENAME.log"
